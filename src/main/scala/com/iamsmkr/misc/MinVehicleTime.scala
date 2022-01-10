50f11b45269bd64eb52a3a3dee33b79113f5f4a7
iamsmkr <mailtoshivamk@gmail.com>
2022-01-10 23:14:15 +0530

    rid duplicates

--- src/main/scala/com/iamsmkr/misc/MinVehicleTime.scala
+++ src/main/scala/com/iamsmkr/misc/MinVehicleTime.scala
@@ -1,5 +1,8 @@
 package com.iamsmkr.misc
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * Input:
  * package_weight array
@@ -51,27 +54,66 @@ package com.iamsmkr.misc
 object MinVehicleTime extends App {
 
   def findMinVehicleTime(pw: Array[Int], vc: Array[Int]): Int = {
-    val r = vc.filter(_ > 0).map(c => pw.filter(_ <= c).sorted.reverse).filter(_.length > 0)
+
+    // This map is used to eliminate duplicate Arrays that are more than the number of elements in a particular Array.
+    // For instance, Array(Array(2, 2), Array(2, 2)) should result in Array(Array(2, 2), Array(2, 2)) but
+    // Array(Array(2, 2), Array(2, 2), Array(2, 2)) should also result in Array(Array(2, 2), Array(2, 2)).
+    val m: mutable.Map[String, (Int, Int)] = mutable.Map()
+
+    val r = vc.filter(_ > 0).map { c =>
+      val p = ArrayBuffer(pw.filter(_ > 0).filter(_ <= c).sorted.reverse: _*)
+      val str = p.mkString("")
+      if (m.isDefinedAt(str)) {
+        if (m(str)._2 == m(str)._1) ArrayBuffer[Int]()
+        else {
+          m.put(str, (m(str)._1 + 1, m(str)._2))
+          p
+        }
+      } else {
+        m.put(str, (1, p.length))
+        p
+      }
+    }.filter(_.nonEmpty)
 
     var shipped = Array[Int]()
+    var shippedPerIter = Array[Int]()
     var trips = 0
 
-    while ((pw.filter(_ <= vc.max) diff shipped).nonEmpty) {
-      r.foreach { arr =>
-        val pending = arr diff shipped
-        if (pending.nonEmpty) shipped = shipped :+ pending.head
+    var counter = 0
+
+    val shippable = pw.filter(_ <= vc.max)
+
+    while ((shippable diff shipped).nonEmpty) {
+      val k = r.map(_.--=(shippedPerIter)).filter(_.nonEmpty)
+
+      shippedPerIter = Array[Int]()
+
+      k.foreach { arr =>
+        counter = counter + 1
+        val pending = arr diff shippedPerIter
+        if (pending.nonEmpty) {
+          shipped = shipped :+ pending.head
+          shippedPerIter = shippedPerIter :+ pending.head
+        }
       }
 
       trips += 1
     }
 
+    println(counter)
+
     val returnTrips = if (trips > 1) trips - 1 else 0
     trips + returnTrips
   }
 
-  assert(findMinVehicleTime(Array(10, 10, 10, 10), Array(100, 100)) == 3)
-  assert(findMinVehicleTime(Array(10, 10, 10, 10), Array(100)) == 7)
-  assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 5)) == 5)
-  assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 25)) == 3)
-  assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 25, 20, 5)) == 1)
+    assert(findMinVehicleTime(Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1), Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)) == 1)
+    assert(findMinVehicleTime(Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1), Array(10, 2, 1)) == 15)
+    assert(findMinVehicleTime(Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1), Array(11, 1)) == 17)
+    assert(findMinVehicleTime(Array(19, 2, 2, 2, 2), Array(29, 3, 3, 3, 3)) == 1)
+    assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 3, 3, 3, 3, 3, 3, 3)) == 5)
+    assert(findMinVehicleTime(Array(10, 10, 10, 10), Array(100, 100)) == 3)
+    assert(findMinVehicleTime(Array(10, 10, 10, 10), Array(100)) == 7)
+    assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 5)) == 5)
+    assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 25)) == 3)
+    assert(findMinVehicleTime(Array(10, 2, 16, 19), Array(29, 25, 20, 5)) == 1)
 }
